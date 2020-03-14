39fa64dcda69c01cd179c9fd05baecbef9456ad0
Dametrik Fick <dametrik.fick@gmail.com>
2020-03-14 03:51:16 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,5 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
+  my_hash = {"name" => "Dametrik", "age" => 25, "weight" => 175}
 
   
 end
